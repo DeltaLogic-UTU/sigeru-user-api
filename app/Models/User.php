1f4a8f138e5ddb70d9cefe824346313fbfed3c1b
NachoUru94 <124197288+NachoUru94@users.noreply.github.com>
2026-09-15 22:10:49 -0300

    feat(auth): actualizar estructura de DB e implementar modelos Eloquent con JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,21 +7,29 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+
+
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
+    protected $table = 'credenciales'; 
+    protected $primaryKey = 'id_credenciales'; 
+    public $timestamps = false;
     protected $fillable = [
-        'name',
-        'email',
+        'id_usuario',
+        'cedula',
         'password',
+        'ultimo_login',
+        'token',
     ];
 
     /**
@@ -31,7 +39,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        
     ];
 
     /**
@@ -42,8 +50,25 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
+    /** 
+     * datos guardados dentro del payload del JWT
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [
+            'id_usuario' => $this->id_usuario,
+            'cedula' => $this->cedula,
+        ];
+    }
+    /**
+     * Relación con tabla principal Usuarios
+     */
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'id_usuario', 'id_usuario');
+    }
+
 }
